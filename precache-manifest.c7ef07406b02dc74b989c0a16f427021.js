23e4b467ebaa7e45c9cf13de78873c8cd8c3eba3
elenapushenko <helenekisselova@gmail.com>
2020-01-23 18:24:52 +0300

    Updates

---
+++ precache-manifest.c7ef07406b02dc74b989c0a16f427021.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a7f482f884c245c65ab59c6f54d113",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fba7b63604c74c7aee96",
+    "url": "/portfolio/static/css/main.802d83eb.chunk.css"
+  },
+  {
+    "revision": "3c2f7e64c63fd2031fb9",
+    "url": "/portfolio/static/js/2.60d94634.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.60d94634.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fba7b63604c74c7aee96",
+    "url": "/portfolio/static/js/main.7a1f0583.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
